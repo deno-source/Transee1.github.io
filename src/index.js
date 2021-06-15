ba9b0ef988cd378a008af349efad89f2f7d50278
transee <1945350177@qq.com>
2021-06-15 16:17:47 +0800

    test

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ function App() {
             <Button type="primary">Button</Button>
     </div>
  }
-ReactDOM.render( <App/> , document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
